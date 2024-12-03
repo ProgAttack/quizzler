fb9b36ce925b883b43ed90d08525142e712c0e39
Giancarlo Kramer <92767138+ProgAttack@users.noreply.github.com>
2024-12-03 08:57:09 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+import 'question.dart';
+
+
+QuizBrain quizBrain = QuizBrain();
+
+void main() => runApp(const Quizzler());
+
+class Quizzler extends StatelessWidget {
+  const Quizzler({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = []; 
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+         Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center, 
+                style: const TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero),
+                  backgroundColor: Colors.green),
+              child: const Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                
+                  bool? correctAnswer = quizBrain.getCorrectAnswer();
+
+                  if (correctAnswer == true) {
+                    print('user got it right!');
+                    
+                  } else {
+                    print('user got it wrong');
+                  }
+
+                setState(() {                  
+                  quizBrain.nextQuestion();
+                });
+                
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero),
+                  backgroundColor: Colors.red),
+              child: const Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+
+                bool? correctAnswer = quizBrain.getCorrectAnswer();
+
+                  if (correctAnswer == false) {
+                    print('user got it right!');
+                    
+                  } else {
+                    print('user got it wrong');
+                  }
+
+                setState(() {
+                  quizBrain.nextQuestion();
+                });
+                
+              },
+            ),
+          ),
+        ),
+        //TODO: Add a Row here as your score keeper
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
